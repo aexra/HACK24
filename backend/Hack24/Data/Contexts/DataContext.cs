882984c6b8ee5913fa1fa7787beccce98de75c8d
Никита Фомин <defdXs@yandex.ru>
2024-10-25 20:56:21 +0300

    Added NOT STABLE backend project

---
+++ backend/Hack24/Data/Contexts/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web.Data.Contexts;
+
+public class DataContext : DbContext
+{
+    public string DbPath { get; }
+
+    public DataContext()
+    {
+        DbPath = "Data/Databases/Data.db";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
---
+++ backend/Hack24/Data/Models/User.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Web.Data.Models;
+
+public class User : IdentityUser
+{
+}

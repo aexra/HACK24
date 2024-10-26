9b027c701c8525743fce7d63d4edda5441b6d1ff
egocucumber <lirik0524@mail.ru>
2024-10-26 08:30:50 +0300

    Добавлены базовые компоненты

---
+++ frontend/src/UI/Text/Textmain.jsx
@@ -0,0 +1,11 @@
+import './Text.css';
+
+const Textmain = ({ children }) => {
+    return (
+        <span className="text-main">
+            {children}
+        </span>
+    );
+};
+
+export default Textmain;
---
+++ frontend/src/components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Landing.module.css'
+
+const Landing = ({children, className, header}) => {
+    return (
+        <div className={`${classes.Landing} ${className}`}>
+            
+        </div>
+    );
+};
+
+export default Landing;

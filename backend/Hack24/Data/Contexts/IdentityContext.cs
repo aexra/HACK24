edd14b4a1633907732b71c14a674d957c0fa1a6f
Никита Фомин <defdXs@yandex.ru>
2024-10-26 12:25:51 +0300

    Fixed missed tables & userid reference in UserTeam

--- backend/Hack24/Data/Contexts/IdentityContext.cs
+++ backend/Hack24/Data/Contexts/IdentityContext.cs
@@ -9,18 +9,31 @@ public class IdentityContext : IdentityDbContext<User>
 {
     public DbSet<ChallengeType> ChallengeTypes { get; set; }
     public DbSet<CompletedSoloChallenge> CompletedSoloChallenges { get; set; }
+    public DbSet<CompletedTeamChallenge> CompletedTeamChallenges { get; set; }
     public DbSet<ImageForRequestToCompleteSoloChallenge> ImagesForRequestToCompleteSoloChallenge { get; set; }
+    public DbSet<ImageForRequestToCompleteTeamChallenge> ImagesForRequestToCompleteTeamChallenge { get; set; }
     public DbSet<Level> Levels { get; set; }
     public DbSet<Notification> Notifications { get; set; }
     public DbSet<Post> Posts { get; set; }
     public DbSet<RepeatForSoloChallenge> RepeatsForSoloChallenge { get; set; }
     public DbSet<RepeatForSoloSelfChallenge> RepeatsForSoloSelfChallenge { get; set; }
+    public DbSet<RepeatForTeamChallenge> RepeatsForTeamChallenge { get; set; }
+    public DbSet<RepeatForTeamSelfChallenge> RepeatsForTeamSelfChallenge { get; set; }
     public DbSet<RequestToCompleteSoloChallenge> RequestsToCompleteSoloChallenge { get; set; }
+    public DbSet<RequestToCompleteTeamChallenge> RequestsToCompleteTeamChallenge { get; set; }
     public DbSet<SoloChallenge> SoloChallenges { get; set; }
+    public DbSet<SoloChallengeCatalog> SoloChallengeCatalogs { get; set; }
     public DbSet<SoloChallengeExpPerPlace> SoloChallengeExpsPerPlace { get; set; }
     public DbSet<SoloSelfChallenge> SoloSelfChallenges { get; set; }
     public DbSet<SoloSelfChallengeCatalog> SoloSelfChallengeCatalogs { get; set; }
-
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<TeamChallenge> TeamChallenges { get; set; }
+    public DbSet<TeamChallengeCatalog> TeamChallengeCatalogs { get; set; }
+    public DbSet<TeamChallengeExpPerPlace> TeamChallengeExps { get; set; }
+    public DbSet<TeamSelfChallenge> TeamSelfChallenges { get; set; }
+    public DbSet<TeamSelfChallengeCatalog> TeamSelfChallengeCatalogs { get; set; }
+    public DbSet<TeamType> TeamTypes { get; set; }
+    public DbSet<UserTeam> UserTeams { get; set; }
 
     public string DbPath { get; }
 
--- backend/Hack24/Data/Models/UserTeam.cs
+++ backend/Hack24/Data/Models/UserTeam.cs
@@ -9,7 +9,7 @@ namespace Hack24.Data.Models;
 public class UserTeam
 {
     [Key, Column(Order = 0)]
-    public int UserId { get; set; }
+    public string UserId { get; set; }
 
     [Key, Column(Order = 1)]
     public int TeamId { get; set; }
